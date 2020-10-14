56164591c3b33dce0742656309d3e516056b1e07
wangxf <wangxf0@hotmail.com>
2020-10-14 21:48:23 +0800

    sql

--- src/main/resources/wxfer.sql
+++ src/main/resources/wxfer.sql
@@ -1,71 +1,47 @@
-/*
-Navicat MySQL Data Transfer
+create table sys_user
+(
+    id          varchar(36) charset utf8  not null comment '主键',
+    user_name   varchar(100) charset utf8 null comment '登录账号',
+    real_name   varchar(100) charset utf8 null comment '真实姓名',
+    password    varchar(255) charset utf8 null comment '密码',
+    salt        varchar(45) charset utf8  null comment 'md5密码盐',
+    avatar      varchar(255) charset utf8 null comment '头像',
+    birthday    datetime                  null comment '生日',
+    sex         tinyint(1)                null comment '性别(0-默认未知,1-男,2-女)',
+    email       varchar(45) charset utf8  null comment '电子邮件',
+    phone       varchar(45) charset utf8  null comment '电话',
+    org_code    varchar(64) charset utf8  null comment '机构编码',
+    status      tinyint(1)                null comment '状态',
+    del_flag    tinyint(1)                null comment '删除状态(0-正常,1-已删除)',
+    telephone   varchar(45) charset utf8  null comment '座机号',
+    create_by   varchar(32) charset utf8  null comment '创建人',
+    create_time datetime                  null comment '创建时间',
+    update_by   varchar(32) charset utf8  null comment '更新人',
+    update_time datetime                  null comment '更新时间',
+    constraint sys_user_id__uindex
+        unique (id)
+)
+    comment '用户表' charset = utf8mb4;
 
-Source Server         : 103.86.47.222
-Source Server Version : 80020
-Source Host           : 103.86.47.222:3306
-Source Database       : wxfer
+alter table sys_user
+    add primary key (id);
 
-Target Server Type    : MYSQL
-Target Server Version : 80020
-File Encoding         : 65001
+create table nav_web_main
+(
+    id          varchar(36)                             not null comment '主键'
+        primary key,
+    sys_user_id varchar(36)                             null comment '用户外键',
+    name        varchar(120) collate utf8mb4_general_ci null comment '网站名称',
+    url         varchar(255) collate utf8mb4_general_ci null comment '网站地址',
+    icon        varchar(50) collate utf8mb4_general_ci  null comment '网站图标',
+    status      tinyint                                 null comment '状态，0正常，1停用',
+    sort        int                                     null comment '排序',
+    create_by   varchar(32)                             null comment '创建人',
+    create_time datetime                                null comment '创建时间',
+    update_by   varchar(32)                             null comment '更新人',
+    update_time datetime                                null comment '更新时间',
+    constraint sys_user_id
+        foreign key (sys_user_id) references sys_user (id)
+)
+    comment '网站列表' charset = utf8;
 
-Date: 2020-09-03 10:17:56
-*/
-
-SET FOREIGN_KEY_CHECKS=0;
-
--- ----------------------------
--- Table structure for nav_web_main
--- ----------------------------
-DROP TABLE IF EXISTS `nav_web_main`;
-CREATE TABLE `nav_web_main` (
-  `id` varchar(36) NOT NULL,
-  `sys_user_id` varchar(36) DEFAULT NULL,
-  `name` varchar(120) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `icon` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `status` tinyint DEFAULT NULL,
-  `create_by` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '创建人',
-  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
-  `update_by` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '更新人',
-  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
-  PRIMARY KEY (`id`),
-  KEY `sys_user_id` (`sys_user_id`),
-  CONSTRAINT `sys_user_id` FOREIGN KEY (`sys_user_id`) REFERENCES `sys_user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of nav_web_main
--- ----------------------------
-
--- ----------------------------
--- Table structure for sys_user
--- ----------------------------
-DROP TABLE IF EXISTS `sys_user`;
-CREATE TABLE `sys_user` (
-  `id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '主键',
-  `username` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '登录账号',
-  `realname` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '真实姓名',
-  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '密码',
-  `salt` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT 'md5密码盐',
-  `avatar` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '头像',
-  `birthday` datetime DEFAULT NULL COMMENT '生日',
-  `sex` tinyint(1) DEFAULT NULL COMMENT '性别(0-默认未知,1-男,2-女)',
-  `email` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '电子邮件',
-  `phone` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '电话',
-  `org_code` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '机构编码',
-  `status` tinyint(1) DEFAULT NULL COMMENT '状态',
-  `del_flag` tinyint(1) DEFAULT NULL COMMENT '删除状态(0-正常,1-已删除)',
-  `telephone` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '座机号',
-  `create_by` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '创建人',
-  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
-  `update_by` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '更新人',
-  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `sys_user_id__uindex` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户表';
-
--- ----------------------------
--- Records of sys_user
--- ----------------------------
